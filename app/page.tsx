2331fd132eb7159aabd1614ea91dcf8f81f6a03a
Vishal K Setti <vishalsetti2000@gmail.com>
2025-05-18 19:48:15 -0400

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             Contact Me
           </a>
           <a href="/resume.pdf" download className="border border-blue-600 text-blue-600 px-6 py-3 rounded-lg hover:bg-blue-50">
-            Download Resume
+            Download My Resume
           </a>
         </div>
       </div>
